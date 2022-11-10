d92dc7562882a46a3f2e118b0a2428e9dfb88ccf
tnduong2807 <tnduong@ucsd.edu>
2022-11-09 16:38:23 -0800

    new

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,30 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.*;
 
 public class TestListExamples {
-  // Write your grading tests here!
+  public void testFilter() {
+    List<String> list = Arrays.asList();
+    List<String> exp = Arrays.asList()
+    StringChecker sc = new StringChecker() {
+      public boolean checkString(String s) {
+        if (s.contains("Test")) {
+          return true;
+        }
+        return false;
+      }
+    };
+    for (int i = 0; i < list.size(); i++) {
+      list.add("Test" + i);
+    }
+    list.add(3, "banana");
+    exp.add("banana");
+    ListExamples.filter(list, sc);
+    assertEquals(exp, sc);
+  }
+
 }
