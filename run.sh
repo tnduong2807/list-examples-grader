0112c4ea19dca05a8abc2a79740fe00d82ea74b1
tnduong2807 <tnduong@ucsd.edu>
2022-11-27 22:43:13 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,2 @@
-MAIN=GradeServer
-PATH=student-submission
-
-rm -f *.class
-
-if [ ! -f $MAIN.java ]; then
-    echo "No $MAIN.java file found"
-    exit 1
-fi
-
-javac Server.java $MAIN.java
-java $MAIN $1
+javac Server.java GradeServer.java
+java GradeServer $1
